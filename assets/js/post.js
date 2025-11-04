53f6638f7b99e0152f8e80051a9aecf91636207f
Abubaker <abubakerahmadi45@gmail.com>
2025-11-04 11:45:38 -0500

    new ppt

--- assets/js/post.js
+++ assets/js/post.js
@@ -9,9 +9,16 @@
   const posts = await res.json();
   const post = posts.find(p => String(p.id) === String(id)) || posts[0];
 
+  const topLink = post.presentationUrl ? `
+    <div class="presentation-cta">
+      <a class="btn" href="${post.presentationUrl}" target="_blank" rel="noopener">View presentation (Canva)</a>
+    </div>
+  ` : '';
+
   wrap.innerHTML = `
     <h1>${post.title}</h1>
     <div class="meta">${new Date(post.date).toLocaleString()}</div>
+    ${topLink}
     <article>${post.html}</article>
   `;
 })();
